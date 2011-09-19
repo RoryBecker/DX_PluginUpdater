22311c4e61cf5321840d3b10953fc6e65c876385
Rory Becker <RoryBecker@gmail.com>
2011-09-19 21:00:11 +0100

    - Pulled content back up from BaseRemotePluginProvider into CommunityPluginProvider.

--- DX_PluginUpdater/Managers/PluginProviders/CommunityPluginProvider.vb
+++ DX_PluginUpdater/Managers/PluginProviders/CommunityPluginProvider.vb
@@ -4,23 +4,52 @@ Imports System.Collections.Generic
 Imports System
 Public Class CommunityPluginProvider
     Inherits BaseRemotePluginProvider
-
-
+#Region "Fields"
+    Private ReadOnly mRootDownloadFolder As String = "/DevExpress/Community/Plugins/"
+    Private ReadOnly mRemoteBasePluginFolder As String = "http://www.rorybecker.co.uk" & mRootDownloadFolder
+    Private Const REGEX_SpecificPluginZipFile As String = "(?<Plugin>({0})_(?<Version>\d+)\.zip"">"
     Private Const REGEX_PluginZipFile As String = "(?<Plugin>(\w|-|_)+)_(?<Version>\d+)\.zip"">"
+#End Region
 
     Private Function GetRootFolderMatches() As MatchCollection
-        Dim Content = WebManager.GetUrlContentAsString(RemoteBasePluginFolder)
-        Dim regex As Regex = New Regex(String.Format("<A HREF=""{0}(?<Plugin>(\w|-|_)+)/"">", RootDownloadFolder), RegexOptions.CultureInvariant Or RegexOptions.Compiled)
+        Dim Content = WebManager.GetUrlContentAsString(mRemoteBasePluginFolder)
+        Dim regex As Regex = New Regex(String.Format("<A HREF=""{0}(?<Plugin>(\w|-|_)+)/"">", mRootDownloadFolder), RegexOptions.CultureInvariant Or RegexOptions.Compiled)
         Return From match In regex.Matches(Content)
     End Function
-
-    Public Function GetPluginReferencesQuick() As IEnumerable(Of RemotePluginRef)
-        Dim Plugins As New List(Of RemotePluginRef)()
-        For Each Match In GetRootFolderMatches()
-            Dim PluginName = Match.Groups("Plugin").Value
-            Plugins.Add(New RemotePluginRef(PluginName, RemoteBasePluginFolder & PluginName))
+    Private Function GetPluginFolderUrl(ByVal PluginName As String) As String
+        Return String.Format("{0}{1}", mRemoteBasePluginFolder, PluginName)
+    End Function
+    Private Function ExtractZipReferences(ByVal Content As String, ByVal Regex As System.Text.RegularExpressions.Regex) As RemotePluginRef
+        Dim LatestPlugin As RemotePluginRef = Nothing
+        For Each Match In Regex.Matches(Content)
+            Dim Plugin As RemotePluginRef = New RemotePluginRef(Match.Groups("Plugin").Value, GetPluginFolderUrl(Match.Groups("Plugin").Value), CInt(Match.Groups("Version").Value))
+            If LatestPlugin Is Nothing OrElse Plugin.Version > LatestPlugin.Version Then
+                LatestPlugin = Plugin
+            End If
         Next
-        Return Plugins
+        Return LatestPlugin
+    End Function
+    Private Function GetSpecificPluginRegex(ByVal PluginName As String) As System.Text.RegularExpressions.Regex
+        Return New System.Text.RegularExpressions.Regex(REGEX_SpecificPluginZipFile, RegexOptions.CultureInvariant Or RegexOptions.Compiled)
+    End Function
+#Region "GetPluginReferences"
+    Public Function GetPluginReference(ByVal PluginName As String) As RemotePluginRef
+        Dim URL = GetPluginFolderUrl(PluginName)
+        Dim Content As String = String.Empty
+        If Not WebManager.ContentIsNot404(URL, Content) Then
+            Return Nothing
+        End If
+        Return ExtractZipReferences(Content, GetSpecificPluginRegex(PluginName))
+    End Function
+    Public Overloads Function GetPluginReferences(ByVal LocalPlugins As IEnumerable(Of PluginRef)) As IEnumerable(Of RemotePluginRef)
+        Dim Results As New List(Of RemotePluginRef)
+        For Each PluginRef As PluginRef In LocalPlugins
+            Dim RemoteReference = GetPluginReference(PluginRef.PluginName)
+            If RemoteReference IsNot Nothing Then
+                Results.Add(RemoteReference)
+            End If
+        Next
+        Return Results
     End Function
     Public Overrides Function GetPluginReferences() As IEnumerable(Of RemotePluginRef)
         Dim Plugins As New List(Of RemotePluginRef)
@@ -32,4 +61,18 @@ Public Class CommunityPluginProvider
         Next
         Return Plugins
     End Function
+    Public Function GetPluginReferencesQuick() As IEnumerable(Of RemotePluginRef)
+        Dim Plugins As New List(Of RemotePluginRef)()
+        For Each Match In GetRootFolderMatches()
+            Dim PluginName = Match.Groups("Plugin").Value
+            Plugins.Add(New RemotePluginRef(PluginName, mRemoteBasePluginFolder & PluginName))
+        Next
+        Return Plugins
+    End Function
+#End Region
+    Public Function GetPluginNames() As IEnumerable(Of String)
+        Return From Plugin As RemotePluginRef In GetPluginReferences() _
+            Select Plugin.PluginName
+    End Function
+
 End Class
