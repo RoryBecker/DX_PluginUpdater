dbb22c5f75d389dac6e2efab13cf0699d56ffb28
Rory Becker <RoryBecker@gmail.com>
2011-09-19 19:10:56 +0100

    - Separated RemotePluginRef from PluginRef.... Again.

---
+++ DX_PluginUpdater/Structures/RemotePluginRef.vb
@@ -0,0 +1,40 @@
+
+Public Class RemotePluginRef
+    Inherits PluginRef
+#Region "Fields"
+    ReadOnly mRemoteFolderUrl As String
+#End Region
+#Region "Construction"
+    Public Sub New(ByVal PluginName As String,
+                   Optional ByVal RemoteFolderUrl As String = "", _
+                   Optional ByVal Version As Integer = 0)
+        MyBase.new(PluginName, Version)
+        mRemoteFolderUrl = RemoteFolderUrl
+    End Sub
+#End Region
+#Region "Properties"
+    Public ReadOnly Property RemoteFolderUrl() As String
+        Get
+            Return mRemoteFolderUrl
+        End Get
+    End Property
+    Public ReadOnly Property RemoteZipFileUrl() As String
+        Get
+            Return String.Format("{0}/{1}", mRemoteFolderUrl, ZipFilename)
+        End Get
+    End Property
+    Public ReadOnly Property ZipFilename() As String
+        Get
+            Return String.Format("{0}_{1}.zip", PluginName, Version)
+        End Get
+    End Property
+#End Region
+
+    Private Function Serialize() As String
+        Return <Reference>
+                   <Name>PluginName</Name>
+                   <Version></Version>
+               </Reference>.ToString
+    End Function
+
+End Class
