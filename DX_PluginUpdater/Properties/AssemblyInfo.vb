c95b115e19f1e04632dbe074357f71cb147e0420
Rory Becker <RoryBecker@gmail.com>
2011-10-20 11:47:17 +0100

    - Increased version to 1.0.0.11.

--- DX_PluginUpdater/Properties/AssemblyInfo.vb
+++ DX_PluginUpdater/Properties/AssemblyInfo.vb
@@ -34,5 +34,5 @@ Imports DevExpress.CodeRush.Common
 ' by using the '*' as shown below:
 ' <Assembly: AssemblyVersion("1.0.*")> 
 
-<Assembly: AssemblyVersion("1.0.0.10")> 
-<Assembly: AssemblyFileVersion("1.0.0.10")> 
+<Assembly: AssemblyVersion("1.0.0.11")> 
+<Assembly: AssemblyFileVersion("1.0.0.11")> 
